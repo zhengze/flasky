8ea4e8d7d49c4217a6c21e52ff190710f7fb5b02
zhengze <zhang_zhze@sina.com>
2015-08-18 00:26:15 +0800

    add test_empty_db, test_login_logout

--- zhenmiao_tests.py
+++ zhenmiao_tests.py
@@ -15,5 +15,28 @@ class zhenmiaoTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(zhenmiao.app.config['DATABASE'])
 
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert 'No entries here so far' in rv.data
+
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
+    def test_login_logout(self):
+        rv = self.login('admin', 'default')
+        assert 'You were logged in' in rv.data
+        rv = self.logout()
+        assert 'You were logged out' in rv.data
+        rv = self.login('adminx', 'default')
+        assert 'Invalid username' in rv.data
+        rv = self.login('admin', 'defaultx')
+        assert 'Invalid password' in rv.data
+
 if __name__ == '__main__':
     unittest.main()
