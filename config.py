c25575be31aa0ce88160a27b8bfa0c2a9a5dfc7c
zhengze <zhang_zhze@sina.com>
2015-08-18 03:45:18 +0800

    add development configuration

--- config.py
+++ config.py
@@ -1,13 +1,20 @@
 # configuration
 
-class TestingConfig(object):
-    DATABASE = '/tmp/zhenmiao.db'
-    DEBUG = True
+class Config(object):
+    DEBUG = False
     SECRET_KEY = '78b32ed2bbb83f84d3d067ce3463d2aa943c7609'
+    CSRF_ENABLED = True
+    
+class TestingConfig(Config):
+    DATABASE = '/tmp/zhenmiao.db'
     USERNAME = 'admin'
     PASSWORD = '1234'
-    CSRF_ENABLED = True
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
 
 config = {
-    'testing': TestingConfig
+    'testing': TestingConfig,
+    'development': DevelopmentConfig,
 }
