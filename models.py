67b60d9839f79df59f12e6ab470f191b9c9fdd4f
zhengze <zhang_zhze@sina.com>
2015-08-25 03:20:34 +0800

    add Entries

--- models.py
+++ models.py
@@ -1,18 +1,28 @@
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Table, Column, Integer, String, Text
 from sqlalchemy.orm import mapper
 from database import db_session, Base
 
-class User(Base):
-    __tablename__ = 'users'
+#class User(Base):
+#    __tablename__ = 'users'
+#    id = Column(Integer, primary_key=True)
+#    name = Column(String(50), unique=True)
+#    email = Column(String(120), unique=True)
+#
+#    def __init__(self, name=None, email=None):
+#        self.name = name
+#        self.email = email
+#
+#    def __repr__(self):
+#        return '<User %r>' % (self.name)
+
+class Entries(Base):
+    __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    email = Column(String(120), unique=True)
-
-    def __init__(self, name=None, email=None):
-        self.name = name
-        self.email = email
-
+    title = Column(String(50)) 
+    text = Column(Text)
+    
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return '<Entry %r>' %(self.title)
+
 
     
