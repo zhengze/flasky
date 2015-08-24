c7deea8c145b7a69a67427be715468e370f21846
zhengze <zhang_zhze@sina.com>
2015-08-25 03:22:14 +0800

    add before_request

--- views.py
+++ views.py
@@ -4,10 +4,14 @@ import sqlite3
 from flask import render_template, abort, redirect, url_for, make_response, \
     g, request, session, flash
 from contextlib import closing
+from flask.ext.login import LoginManager, logout_user, current_user
 from forms import LoginForm
 from main import app
 from database import db_session
-from models import User
+from models import Entries
+
+login_manager = LoginManager()
+login_manager.setup_app(app)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
@@ -37,6 +41,13 @@ def teardown_request(exception):
 def shutdown_session(exception=None):
     db_session.remove()
 
+@app.before_request
+def before_request():
+    g.user = current_user
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.get(userid)
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -48,8 +59,9 @@ def page_not_found(error):
 
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    #cur = g.db.execute('select title, text from entries order by id desc')
+    #entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    entries = Entries.query.all()
     return render_template('show_entries.html', entries=entries)
 
 
@@ -72,13 +84,14 @@ def login():
         form = LoginForm(request.form)
         username = form.username.data
         password = form.password.data
-
         if username != app.config['USERNAME']:
             error = 'Invalid username'
         elif password != app.config['PASSWORD']:
             error = 'Invalid password'
         else:
-            session['logged_in'] = True
+            #session['logged_in'] = True
+            g.user = current_user
+            print dir(current_user)
             flash('You were logged in')
             return redirect(url_for('show_entries'))
     else:
@@ -89,7 +102,8 @@ def login():
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None)
+    #session.pop('logged_in', None)
+    logout_user()
     flash('You were logged out')
     return redirect(url_for('show_entries'))
 
