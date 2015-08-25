cbb7798af8eb5ac25a5e86bddba843c2e311711b
zhengze <zhang_zhze@sina.com>
2015-08-25 09:21:45 +0800

    User表增加password, role

--- models.py
+++ models.py
@@ -1,19 +1,35 @@
-from sqlalchemy import Table, Column, Integer, String, Text
+from sqlalchemy import Table, Column, Integer, String, Text, Unicode, SmallInteger
 from sqlalchemy.orm import mapper
 from database import db_session, Base
+from flask.ext.login import LoginManager, UserMixin
+from main import app
 
-#class User(Base):
-#    __tablename__ = 'users'
-#    id = Column(Integer, primary_key=True)
-#    name = Column(String(50), unique=True)
-#    email = Column(String(120), unique=True)
-#
-#    def __init__(self, name=None, email=None):
-#        self.name = name
-#        self.email = email
-#
-#    def __repr__(self):
-#        return '<User %r>' % (self.name)
+ROLE_USER = 0
+ROLE_ADMIN = 1
+
+login_manager = LoginManager()
+login_manager.setup_app(app)
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.get(userid)
+
+
+class User(Base, UserMixin):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(Unicode(40), nullable=False)
+    email = Column(String(120), unique=True)
+    role = Column(SmallInteger, default=ROLE_USER)
+
+    def __init__(self, username=None, password=None, email=None):
+        self.username = username
+        self.password = password
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
 
 class Entries(Base):
     __tablename__ = 'entries'
