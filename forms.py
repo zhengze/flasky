d7314bb05b0d528be22b202cccc78b2e3ba20bbc
zhengze <zhang_zhze@sina.com>
2015-08-25 09:20:38 +0800

    add RegistrationForm, EntryForm

--- forms.py
+++ forms.py
@@ -1,7 +1,22 @@
 #coding:utf8
 
-from wtforms import Form, BooleanField, TextField, PasswordField, validators
+from wtforms import Form, BooleanField, TextField, PasswordField, \
+    TextAreaField, validators
+
+class RegistrationForm(Form):
+    username = TextField('Username', [validators.Length(min=4, max=25)])
+    email = TextField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+            validators.Required(),
+            validators.EqualTo('confirm', message='Passwords must match')
+            ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.Required()])
 
 class LoginForm(Form):
-    username = TextField('用户名', [validators.Length(min=4, max=25)])
-    password = PasswordField('密码')
+    username = TextField(u'用户名', [validators.Length(min=4, max=25)])
+    password = PasswordField(u'密码')
+
+class EntryForm(Form):
+    title = TextField(u'标题')
+    text = TextAreaField(u'正文', [validators.optional(), validators.length(max=200)])
