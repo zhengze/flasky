28b18a2bc4db6118510bfaa7e86ca9d0582cabe0
zhengze <zhang_zhze@sina.com>
2015-08-27 18:06:06 +0800

    config_name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from config import config
 
-config_name = 'testing'
+config_name = 'development'
 app = Flask(__name__)
 #app.config.from_envvar('ZHENMIAO_SETTINGS', silent=True)
 app.config.from_object(config[config_name])
