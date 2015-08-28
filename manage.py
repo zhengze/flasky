48fdd35b43b938a70a53230c0ff033ac5a520d82
zhengze <zhang_zhze@sina.com>
2015-08-28 21:40:31 +0800

    add manager

--- manage.py
+++ manage.py
@@ -1,6 +1,11 @@
 from init import *
 from views import *
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+app.debug = True
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run('0.0.0.0')
+    manager.run('0.0.0.0')
