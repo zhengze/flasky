41503ffa298136ca5c697e8eaaa3b6de3d437ed8
zhengze <zhanghhd@163.com>
2017-12-17 07:28:29 -0800

    add_option add_command

--- manage.py
+++ manage.py
@@ -1,11 +1,16 @@
 from init import *
 from views import *
-from flask.ext.script import Manager, Server
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask_script import Manager, Server, Shell
+from flask_migrate import Migrate, MigrateCommand
+
+def make_shell_context():
+    return dict(app=app, db=db)
 
 app.debug = True
 manager = Manager(app)
+manager.add_option('-c', '--config', dest='config', required=False)
 manager.add_command('db', MigrateCommand)
+manager.add_command('shell', Shell(make_context=make_shell_context))
 server = Server(host='0.0.0.0', port=5000)
 manager.add_command('runserver', server)
 
