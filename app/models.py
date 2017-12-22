591f521f6e17e480b4e9517387fb73fa3dd089b3
zhengze <zhanghhd@163.com>
2017-12-22 08:51:01 -0800

    db

--- app/models.py
+++ app/models.py
@@ -1,10 +1,5 @@
-#from sqlalchemy import Table, Column, Integer, String, Text, Unicode, SmallInteger
-#from sqlalchemy.orm import mapper
-#from database import db_session, Base
 from flask_login import UserMixin
-from app import login_manager
-from app.db import Table, Column, Integer, String, Text, Unicode, SmallInteger
-
+from app import login_manager, db
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
@@ -17,11 +12,11 @@ def load_user(userid):
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    username = Column(String(50), unique=True, nullable=False)
-    password = Column(Unicode(40), nullable=False)
-    email = Column(String(120), unique=True)
-    role = Column(SmallInteger, default=ROLE_USER)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.Unicode(40), nullable=False)
+    email = db.Column(db.String(120), unique=True)
+    role = db.Column(db.SmallInteger, default=ROLE_USER)
 
     def __init__(self, username=None, password=None, email=None):
         self.username = username
@@ -33,9 +28,9 @@ class User(db.Model, UserMixin):
 
 class Entries(db.Model):
     __tablename__ = 'entries'
-    id = Column(Integer, primary_key=True)
-    title = Column(String(50)) 
-    text = Column(Text)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50)) 
+    text = db.Column(db.Text)
     
     def __repr__(self):
         return '<Entry %r>' %(self.title)
