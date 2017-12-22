21ca371597207d2b09174fea118beb3b1bdb3230
zhengze <zhanghhd@163.com>
2017-12-22 09:10:23 -0800

    app_context push

--- manage.py
+++ manage.py
@@ -5,10 +5,12 @@ from flask_migrate import Migrate, MigrateCommand
 
 config_name = os.getenv("FLASK_CONFIG") or "default"
 app = create_app(config_name)
+app.app_context().push()
 
 
 def make_shell_context():
     return dict(app=app, db=db)
+
 
 manager = Manager(app)
 migrate = Migrate(app, db)
