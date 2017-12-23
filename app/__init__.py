0322ea2e0bdff7d6a8ca813250bf423e9dfb44c4
zhengze <zhanghhd@163.com>
2017-12-23 08:43:23 -0800

    add blueprint auth

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,8 @@ from config import config
 db = SQLAlchemy()
 
 login_manager = LoginManager()
-#login_manager.setup_app(app)
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,7 +20,9 @@ def create_app(config_name):
     login_manager.init_app(app)
 
     from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(main_blueprint, url_prefix="/")
+    app.register_blueprint(auth_blueprint, url_prefix="/auth")
 
     return app
     
