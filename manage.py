ecfcfba0f565cb76c2d4aeb14d0e6c9163cc5c58
zhengze <zhanghhd@163.com>
2017-12-23 08:47:52 -0800

    test function

--- manage.py
+++ manage.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+
 import os
 from app import create_app, db
 from flask_script import Manager, Server, Shell
@@ -7,13 +11,20 @@ config_name = os.getenv("FLASK_CONFIG") or "default"
 app = create_app(config_name)
 app.app_context().push()
 
+migrate = Migrate(app, db)
+manager = Manager(app)
 
+@manager.shell
 def make_shell_context():
     return dict(app=app, db=db)
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittests.TextTestRunner(verbosity=2).run(tests)
 
-manager = Manager(app)
-migrate = Migrate(app, db)
 
 manager.add_option('-c', '--config', dest='config', required=False)
 manager.add_command('db', MigrateCommand)
