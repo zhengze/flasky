6b1bee5013352f43bfd4ba22f7356de545832559
600408 <600408@syswin.com>
2018-01-21 05:18:18 -0800

    add flask-moment,flask-bootstrap,flask-mail

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,20 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_mail import Mail
 from config import config
 
 db = SQLAlchemy()
+moment = Moment()
+mail = Mail()
+bootstrap = Bootstrap()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -17,10 +24,15 @@ def create_app(config_name):
     #config[config_name].init_app(app)
 
     db.init_app(app)
+    moment.init_app(app)
     login_manager.init_app(app)
+    bootstrap.init_app(app)
+    mail.init_app(app)
 
-    from .main import main as main_blueprint
+    from .api import api as api_blueprint                                                         from .main import main as main_blueprint
     from .auth import auth as auth_blueprint
+
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
     app.register_blueprint(main_blueprint, url_prefix="/")
     app.register_blueprint(auth_blueprint, url_prefix="/auth")
 
